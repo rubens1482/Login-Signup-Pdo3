c8afc9c4dbf077a8dc7d14f3b96ce6cfb3646104
rubens1482 <rubenscouto@gmail.com>
2020-12-19 09:55:34 -0300

    Primeiros arquivos do projeto

--- hash.php
+++ hash.php
@@ -6,6 +6,6 @@
  * Beware that DEFAULT may change over time, so you would want to prepare
  * By allowing your storage to expand past 60 characters (255 would be good)
  */
-echo password_hash("Julieta Pontes Viana", PASSWORD_DEFAULT);
+echo password_hash("Rubens", PASSWORD_DEFAULT);
 
 ?>
